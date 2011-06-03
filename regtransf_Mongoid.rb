7c890fde7cddb2336198228cfbb6e1d034e1215c
Jesus Marin <jesusangelm@gmail.com>
2011-06-03 12:16:47 -0430

    first commit

---
+++ regtransf_Mongoid.rb
@@ -0,0 +1,48 @@
+#!/usr/bin/env ruby
+
+require "mongoid"
+
+Mongoid.configure do |config|
+  config.master = Mongo::Connection.new.db("RT_mongoid")
+end
+
+class Registro
+    include Mongoid::Document
+    field :subida, type: Float
+    field :bajada, type: Float
+    field :fecha_hora, typo: String  
+end
+
+def bytestomb bytes
+	mb = bytes.to_f / (1024*1024)
+	mb
+end
+
+interface = "ppp0"
+archivo = File.open('/proc/net/dev', 'r')
+archivo.each_line do |line|
+	#puts line
+	if line =~ /ppp0/
+		#puts line
+		data = line.split('%s:' % interface)[1].split()
+		$rx_bytes = data[0]
+		$tx_bytes = data[8]
+		puts "Recibidos: " + (bytestomb $rx_bytes).to_s
+		puts "Enviados: " + (bytestomb $tx_bytes).to_s
+	end
+end
+
+regsubida = (bytestomb $tx_bytes).to_f
+regbajada = (bytestomb $rx_bytes).to_f
+tiempo = Time.now
+
+registro = Registro.create(subida: regsubida, bajada: regbajada,
+                            fecha_hora: tiempo.to_s)
+
+puts "Registros en la BD " + Registro.count.to_s
+
+cursor = Registro.all
+puts "Total de Subida: " + cursor.sum(:subida).to_s
+puts "Total de Bajada: " + cursor.sum(:bajada).to_s
+total = cursor.sum(:subida) + cursor.sum(:bajada)
+puts "Total Consumido: " + total.to_s
